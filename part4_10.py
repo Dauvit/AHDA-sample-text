acac74a6e587d0fa57b71b07d70f6d7788fb62b3
David King <david.king@open.ac.uk>
2019-02-08 16:58:59 +0000

    Create part4_10.py
    Bonus on looping through all files in a folder

---
+++ part4_10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Python for AHDA.
+
+Part 4, Example 10.
+"""
+
+# looping through all files in a folder
+
+
+from pathlib import Path
+
+INPUT_FOLDER = '.'
+PATTERN = '*.csv'
+
+files_to_read = Path(INPUT_FOLDER).glob(PATTERN)
+for file_to_read in files_to_read:
+    with open(file_to_read, 'r', encoding='utf-8', newline='\n') as f_in:
+        print(file_to_read.name)
